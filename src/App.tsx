2bd5b6e6d1b749c3485fd0f099f89e80f67f0f52
Gordo <matheus.silva31@estudante.ifms.edu.br>
2024-10-14 10:19:18 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+type ProdutoType = {
+  id:number,
+  nome:string,
+  preco:string,
+  descricao:string,
+  imagem:string
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [nome,setNome] = useState("")
+  const [produtos, setProdutos] = useState<ProdutoType []>([
 
+  ])
+  useEffect(()=>{
+    setNome("Diego Barros")
+  },[])
+  fetch("https://one022a-marketplace-39c3.onrender.com/produtos")
+  .then(resposta=>resposta.json())
+  .then(dados=>setProdutos(dados))
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Guilherme Figueiredo Terenciani</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>{nome}</h1>
+     <div className="produtos-container">
+      {
+        produtos.map(produto=>{
+          return(<div className="produto-item">
+            <h1>{produto.nome}</h1>
+            <p>{produto.imagem}</p>
+            <img src={produto.imagem} alt="imagem" />
+            <p>{produto.preco}</p>
+            <p>{produto.descricao}</p>
+          </div>)
+        })
+      }
+     </div>
     </>
   )
 }
